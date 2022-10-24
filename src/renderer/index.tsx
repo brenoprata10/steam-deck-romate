0f65f622b1a41b37339da09dff6faad4804174f8
Breno Prata <brenoprata@hotmail.com>
2022-10-24 14:01:11 +0200

    Remove broken code

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -5,10 +5,3 @@ import './index.scss'
 const container = document.getElementById('root')!
 const root = createRoot(container)
 root.render(<App />)
-
-// calling IPC exposed from preload script
-window.electron.ipcRenderer.once('ipc-example', (arg) => {
-	// eslint-disable-next-line no-console
-	console.log(arg)
-})
-window.electron.ipcRenderer.sendMessage('ipc-example', ['ping'])
