ad9f959940b50850f10e260c74de610ca2eb9588
Breno Prata <brenoprata@hotmail.com>
2022-10-26 14:21:18 +0200

    Remove wrapper component

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,25 @@
+import {useReducer} from 'react'
+import {MemoryRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.scss'
-import MainWrapper from 'renderer/wrapper/MainWrapper.component'
+import {CommonContext, CommonDispatchContext} from './context'
+import Setup from './pages/setup/Setup.component'
+import {INITIAL_STATE, reducer} from './reducer'
 
 export default function App() {
-	return <MainWrapper />
+	const [state, dispatch] = useReducer(reducer, INITIAL_STATE)
+
+	return (
+		<CommonDispatchContext.Provider value={dispatch}>
+			<CommonContext.Provider value={state}>
+				<main className={'main-wrapper'}>
+					<Router>
+						<Routes>
+							<Route path='/' element={<Setup />} />
+							<Route path='/configure-assets' element={<Setup />} />
+						</Routes>
+					</Router>
+				</main>
+			</CommonContext.Provider>
+		</CommonDispatchContext.Provider>
+	)
 }
