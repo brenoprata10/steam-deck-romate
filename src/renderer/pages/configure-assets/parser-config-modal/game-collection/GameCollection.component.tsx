737d7fc2c07383285f8322c89b9c2855ebefaf8a
Breno Prata <brenoprata@hotmail.com>
2022-12-06 19:54:32 +0100

    #5 - Add collapsible platforms to rom modal

--- src/renderer/pages/configure-assets/parser-config-modal/game-collection/GameCollection.component.tsx
+++ src/renderer/pages/configure-assets/parser-config-modal/game-collection/GameCollection.component.tsx
@@ -1,6 +1,9 @@
-import {useCallback} from 'react'
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {useCallback, useState} from 'react'
 import TGame from 'renderer/types/TGame'
 import Checkbox from 'renderer/uikit/checkbox/Checkbox.component'
+import Collapsible from 'renderer/uikit/collapsible/Collapsible.component'
 import styles from './GameCollection.module.scss'
 
 const GameCollection = ({
@@ -14,6 +17,8 @@ const GameCollection = ({
 	onCollectionClick: () => void
 	onGameClick: (game: TGame) => void
 }) => {
+	const [isCollapsed, setIsCollapsed] = useState(true)
+
 	const handleGameClick = useCallback(
 		(game: TGame) => {
 			onGameClick(game)
@@ -24,9 +29,9 @@ const GameCollection = ({
 	const isExcludedCollection = games.some((game) => game.isExcluded)
 
 	return (
-		<div className={styles['parser-config-game-collection']}>
-			<Checkbox id={collection} label={collection} onChange={onCollectionClick} checked={!isExcludedCollection} />
-			{games.map((game) => (
+		<Collapsible
+			isCollapsed={isCollapsed}
+			innerComponent={games.map((game) => (
 				<div className={styles['game-item']} key={`${game.id}-${collection}`}>
 					<Checkbox
 						id={`${game.id}-${collection}`}
@@ -36,7 +41,19 @@ const GameCollection = ({
 					/>
 				</div>
 			))}
-		</div>
+			className={styles['parser-config-game-collection']}
+			innerClassNameWrapper={styles['collection-list']}
+		>
+			<div className={styles['collection-name']}>
+				<FontAwesomeIcon
+					icon={faChevronRight}
+					transform={{rotate: isCollapsed ? 0 : 90}}
+					style={{transition: '.3s'}}
+					onClick={() => setIsCollapsed(!isCollapsed)}
+				/>
+				<Checkbox id={collection} label={collection} onChange={onCollectionClick} checked={!isExcludedCollection} />
+			</div>
+		</Collapsible>
 	)
 }
 
